7b2d0ba3688590d4e377283efac2e7e5030803fe
Luis Fernando Bórquez <luis.borquezu@outlook.com>
2025-05-11 15:20:15 -0700

    Fin de sección 07, testing de aplicación de consola, proyecto 04-MultiplicationApp

--- 04-MultiplicationApp/src/domain/use-cases/create-table.use-case.ts
+++ 04-MultiplicationApp/src/domain/use-cases/create-table.use-case.ts
@@ -18,7 +18,11 @@ export class CreateTable implements ICreateTableUseCase {
         let outputMessage = '';
 
         for (let i = 1; i <= limit; i++) {
-            outputMessage += `${base} x ${i} = ${ (base * i) }\n`;
+            outputMessage += `${base} x ${i} = ${ (base * i) }`;
+
+            if (i < limit) {
+                outputMessage += '\n';
+            }
         }
 
         return outputMessage;
